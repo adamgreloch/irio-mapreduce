895d5bed3ee4590f37feba58a4f2b6f092cae379
mikolaj <mikolajtuchola@gmail.com>
2024-01-04 21:55:18 +0100

    Fixed mr changes.

--- src/main/java/pl/edu/mimuw/mapreduce/batchmanager/BatchManager.java
+++ src/main/java/pl/edu/mimuw/mapreduce/batchmanager/BatchManager.java
@@ -1,21 +1,19 @@
 package pl.edu.mimuw.mapreduce.batchmanager;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import pl.edu.mimuw.mapreduce.Utils;
-import pl.edu.mimuw.mapreduce.common.Batch;
-import pl.edu.mimuw.mapreduce.common.Task;
-import pl.edu.mimuw.mapreduce.healthcheck.Ping;
-import pl.edu.mimuw.mapreduce.healthcheck.PingResponse;
-import pl.edu.mimuw.mapreduce.taskmanager.TaskManagerGrpc;
-import sun.jvm.hotspot.code.Stub;
+import pl.edu.mimuw.proto.batchmanager.BatchManagerGrpc;
+import pl.edu.mimuw.proto.common.Batch;
+import pl.edu.mimuw.proto.common.Response;
+import pl.edu.mimuw.proto.healthcheck.Ping;
+import pl.edu.mimuw.proto.healthcheck.PingResponse;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class BatchManager {
@@ -31,11 +29,7 @@ public class BatchManager {
         private Map<Integer, Boolean> finishedMapping = new ConcurrentHashMap<>();
 
         @Override
-        public void doBatch(Batch request, StreamObserver<BatchManagerDone> responseObserver) {
-            ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",2137).usePlaintext().build(); // TODO set this properly
-            TaskManagerGrpc.TaskManagerFutureStub productServiceAsyncStub = TaskManagerGrpc.newFutureStub(managedChannel);
-
-
+        public void doBatch(Batch request, StreamObserver<Response> responseObserver) {
             throw new RuntimeException("todo");
         }
 
