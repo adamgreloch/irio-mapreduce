626d84b3bdafe550b6a92386545a661c27ccff41
mikolaj <mikolajtuchola@gmail.com>
2024-01-19 19:05:55 +0100

    Implemented ServerBreakerImpl singleton.

--- src/main/java/pl/edu/mimuw/mapreduce/serverbreaker/ServerBreakerImpl.java
+++ src/main/java/pl/edu/mimuw/mapreduce/serverbreaker/ServerBreakerImpl.java
@@ -1,13 +1,37 @@
 package pl.edu.mimuw.mapreduce.serverbreaker;
 
 import io.grpc.stub.StreamObserver;
+import pl.edu.mimuw.mapreduce.Utils;
 import pl.edu.mimuw.proto.common.Response;
+import pl.edu.mimuw.proto.processbreaker.Action;
 import pl.edu.mimuw.proto.processbreaker.Payload;
 import pl.edu.mimuw.proto.processbreaker.ServerBreakerGrpc;
 
-public class ServerBreakerImpl extends ServerBreakerGrpc.ServerBreakerImplBase {
+public final class ServerBreakerImpl extends ServerBreakerGrpc.ServerBreakerImplBase {
+    private static volatile ServerBreakerImpl instance;
+    private Action action;
+
+    private ServerBreakerImpl() {
+        this.action = Action.NONE;
+    }
+
     @Override
     public void executePayload(Payload request, StreamObserver<Response> responseObserver) {
+        this.action = request.getAction();
+        Utils.respondWithSuccess(responseObserver);
+    }
 
+    public static ServerBreakerImpl getInstance() {
+        // https://refactoring.guru/java-dcl-issue
+        ServerBreakerImpl result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized(ServerBreakerImpl.class) {
+            if (instance == null) {
+                instance = new ServerBreakerImpl();
+            }
+            return instance;
+        }
     }
 }
