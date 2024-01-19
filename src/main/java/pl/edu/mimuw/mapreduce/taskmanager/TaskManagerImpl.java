5f9ca8cb80c0afc9c60b0b999a6c9ec0127fcd6d
mikolaj <mikolajtuchola@gmail.com>
2024-01-20 00:15:27 +0100

    Rerun task if it was finished with some error.

--- src/main/java/pl/edu/mimuw/mapreduce/taskmanager/TaskManagerImpl.java
+++ src/main/java/pl/edu/mimuw/mapreduce/taskmanager/TaskManagerImpl.java
@@ -6,7 +6,6 @@ import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.ManagedChannel;
 import io.grpc.protobuf.services.HealthStatusManager;
 import io.grpc.stub.StreamObserver;
-import jdk.jshell.execution.Util;
 import org.apache.commons.io.IOUtils;
 import pl.edu.mimuw.mapreduce.Utils;
 import pl.edu.mimuw.mapreduce.common.ClusterConfig;
@@ -26,17 +25,67 @@ import pl.edu.mimuw.proto.worker.WorkerGrpc;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-
+//private FutureCallback<Response> createCallback(Batch batch, StreamObserver<Response> responseObserver,
+//                                                TaskManagerGrpc.TaskManagerFutureStub taskManagerFutureStub) {
+//    return new FutureCallback<>() {
+//        /** Try to send next Task to TaskManager. */
+//        @Override
+//        public void onSuccess(Response result) {
+//            if (result.getStatusCode() != StatusCode.Ok) {
+//                Response response = Response.newBuilder().setStatusCode(result.getStatusCode()).setMessage(
+//                        "Success but got some internal error.").build();
+//                responseObserver.onNext(response);
+//                responseObserver.onCompleted();
+//                return;
+//            }
+//            // TODO check if every map was executed on input, by checking some value in storage.
+//            // If so set BatchPhase to Reducing.
+//            int doneTaskNr = doneTasks.get(batch);
+//            doneTaskNr++;
+//
+//            if (batchPhases.get(batch).equals(BatchPhase.Mapping)) {
+//                // end of mapping phase
+//                doneTasks.put(batch, 0);
+//                batchPhases.put(batch, BatchPhase.Reducing);
+//            } else {
+//                doneTasks.put(batch, doneTaskNr);
+//            }
+//
+//            Optional<Task> optional = getTask(batch);
+//            if (optional.isEmpty()) {
+//                // batch is finished
+//                responseObserver.onNext(Response.newBuilder().setStatusCode(StatusCode.Ok).build());
+//                responseObserver.onCompleted();
+//                return;
+//            }
+//
+//            ListenableFuture<Response> listenableFuture = taskManagerFutureStub.doTask(optional.get());
+//            Futures.addCallback(listenableFuture, createCallback(batch, responseObserver, taskManagerFutureStub),
+//                    pool);
+//        }
+//
+//        /** Stop processing the batch and send error message. */
+//        @Override
+//        public void onFailure(Throwable t) {
+//            Response response =
+//                    Response.newBuilder().setStatusCode(StatusCode.Err).setMessage(t.getMessage()).build();
+//            responseObserver.onNext(response);
+//            responseObserver.onCompleted();
+//        }
+//    };
+//}
 public class TaskManagerImpl extends TaskManagerGrpc.TaskManagerImplBase implements HealthCheckable {
     private final Storage storage;
     private final ExecutorService pool = Executors.newCachedThreadPool();
     private final ManagedChannel workerChannel;
     private final HealthStatusManager health;
+    private final Integer MAX_ATTEMPT = 3;
 
     public TaskManagerImpl(Storage storage, HealthStatusManager health, String workersUri) {
         this.storage = storage;
@@ -55,13 +104,33 @@ public class TaskManagerImpl extends TaskManagerGrpc.TaskManagerImplBase impleme
                 ClusterConfig.TASK_MANAGERS_URI).awaitTermination();
     }
 
-    private FutureCallback<Response> createWorkerResponseCallback(StreamObserver<Response> responseObserver,
-                                                                  CountDownLatch operationsDoneLatch) {
+    private FutureCallback<Response> createWorkerMapResponseCallback(StreamObserver<Response> responseObserver,
+                                                                     CountDownLatch operationsDoneLatch,
+                                                                     Object request,
+                                                                     int attempt,
+                                                                     WorkerGrpc.WorkerFutureStub workerFutureStub) {
+        if (!(request instanceof DoMapRequest) && !(request instanceof DoReduceRequest)) {
+            throw new AssertionError("Object is neither an instance of DoMapRequest or DoReduceRequest");
+        }
         return new FutureCallback<>() {
             @Override
             public void onSuccess(Response result) {
                 if (result.getStatusCode() == StatusCode.Err) {
-                    throw new RuntimeException("todo: handle worker failure");
+                    if(attempt > MAX_ATTEMPT){
+                        Utils.respondWithThrowable(new RuntimeException("Number of attempts for task was exceeded"), responseObserver);
+                        return;
+                    }
+                    ListenableFuture<Response> listenableFuture;
+                    if (request instanceof DoMapRequest doMapRequest) {
+                        listenableFuture = workerFutureStub.doMap(doMapRequest);
+                    } else {
+                        DoReduceRequest doReduceRequest = (DoReduceRequest) request;
+                        listenableFuture = workerFutureStub.doReduce(doReduceRequest);
+                    }
+                    Futures.addCallback(listenableFuture,
+                            createWorkerMapResponseCallback(responseObserver, operationsDoneLatch, request, attempt + 1, workerFutureStub),
+                            pool);
+                    return;
                 }
                 operationsDoneLatch.countDown();
             }
@@ -82,7 +151,7 @@ public class TaskManagerImpl extends TaskManagerGrpc.TaskManagerImplBase impleme
 
     @Override
     public void healthCheck(Ping request, StreamObserver<PingResponse> responseObserver) {
-        if(Utils.handleServerBreakerHealthCheckAction(responseObserver)){
+        if (Utils.handleServerBreakerHealthCheckAction(responseObserver)) {
             return;
         }
         Utils.LOGGER.trace("Received health check request");
@@ -96,7 +165,7 @@ public class TaskManagerImpl extends TaskManagerGrpc.TaskManagerImplBase impleme
 
     @Override
     public PingResponse internalHealthcheck() {
-        if (Utils.handleServerBreakerInternalHealthCheckAction()){
+        if (Utils.handleServerBreakerInternalHealthCheckAction()) {
             return PingResponse.newBuilder().setStatusCode(HealthStatusCode.Error).build();
         }
         Utils.LOGGER.warn("healthchecking not implemented");
@@ -161,7 +230,7 @@ public class TaskManagerImpl extends TaskManagerGrpc.TaskManagerImplBase impleme
                 var doMapRequest = DoMapRequest.newBuilder().setTask(task).setSplit(split).build();
                 ListenableFuture<Response> listenableFuture = workerFutureStub.doMap(doMapRequest);
                 Futures.addCallback(listenableFuture,
-                        createWorkerResponseCallback(responseObserver, phaseDoneLatch),
+                        createWorkerMapResponseCallback(responseObserver, phaseDoneLatch, doMapRequest),
                         pool);
             }
 
@@ -208,7 +277,7 @@ public class TaskManagerImpl extends TaskManagerGrpc.TaskManagerImplBase impleme
 
                 var doReduceRequest = DoReduceRequest.newBuilder().setTask(task).setFileId(fileId).build();
                 ListenableFuture<Response> listenableFuture = workerFutureStub.doReduce(doReduceRequest);
-                Futures.addCallback(listenableFuture, createWorkerResponseCallback(responseObserver, phaseDoneLatch), pool);
+                Futures.addCallback(listenableFuture, createWorkerReduceResponseCallback(responseObserver, phaseDoneLatch), pool);
             }
 
             try {
